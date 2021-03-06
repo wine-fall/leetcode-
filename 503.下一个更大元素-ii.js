f0e8a50cf20a67816504a98294f6d9ad1d1486cc
fall-wine <wine@mail.bnu.edu.cn>
2021-03-06 16:56:12 +0800

    503 下一个最大元素 tag单调栈

--- 503.下一个更大元素-ii.js
+++ 503.下一个更大元素-ii.js
@@ -10,7 +10,23 @@
  * @return {number[]}
  */
 var nextGreaterElements = function(nums) {
-    
+    const n = nums.length;
+    const res = Array(n).fill(-1);
+    const stack = [];
+    for (let i = 0; i < n * 2; i++) {
+        while (stack.length
+            && nums[stack[stack.length - 1]] < nums[i % n]
+        ) {
+            const curIndex = stack.pop();
+            res[curIndex] = nums[i % n];
+        }
+        stack.push(i % n);
+    }
+    return res;
 };
+
+var r1 = nextGreaterElements([1,5,4,3,2,1]);
+
+console.log(r1);
 // @lc code=end
 
