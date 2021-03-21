9d1edbe87829bac8cfe85cb9c88d6d417018fa55
fall-wine <wine@mail.bnu.edu.cn>
2021-03-21 16:16:58 +0800

    73. 矩阵置零 tag：matrix

---
+++ 73.矩阵置零.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=73 lang=javascript
+ *
+ * [73] 矩阵置零
+ */
+
+// @lc code=start
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+    const m = matrix.length, n = matrix[0].length;
+    const row = new Array(m).fill(false);
+    const col = new Array(n).fill(false);
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] === 0) {
+                row[i] = col[j] = true;
+            }
+        }
+    }
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (row[i] || col[j]) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    return matrix;
+};
+
+var r1 = setZeroes([[1,1,1],[1,0,1],[1,1,1]]);
+console.log(r1);
+// @lc code=end
+
